3c32536a40a163a7217b9d261a85b5a223609262
Pavel <bashmakovhub@gmail.com>
2023-12-28 16:36:18 +0300

    Добавил функционал создания и генерации CSV файлов. Добавил флаг свойства запуска системы. Добавил файл gitignore

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/etnz/permute"
@@ -16,9 +20,11 @@ import (
 
 // ITERATION_LOOP - переменная итераций цикла. Задается с помощью ключа "-i".
 var ITERATION_LOOP int = 10
+var START_PROPERTY_FLAG int = 1
 
 func init() {
 	flag.IntVar(&ITERATION_LOOP, "i", ITERATION_LOOP, "кол-во итераций цикла")
+	flag.IntVar(&START_PROPERTY_FLAG, "sp", START_PROPERTY_FLAG, "св-во старта")
 }
 
 // printScreen - функция вывода на экран
@@ -69,6 +75,7 @@ func readFile(l int) time.Duration {
 		}
 
 		scan := bufio.NewScanner(file)
+		// TODO: почему-то пустое условие if
 		for scan.Scan() {
 			str := scan.Text()
 			if str == "" {
@@ -129,8 +136,8 @@ func openDifferentFiles(l int) time.Duration {
 	return t1.Sub(t0)
 }
 
-// outputFuncDifferentTurn - функция вывода времени выполнения функций в разной последовательностии генерации Excel файла
-func outputFuncDifferentTurn(file *excelize.File) {
+// outputFuncDifferentTurn - функция вывода времени выполнения функций в разной последовательностии, генерации Excel и CSV файлов
+func outputFuncDifferentTurn(file *excelize.File, writerCSV *csv.Writer) {
 	var indexExcle int = 1
 	var funcTime time.Duration
 	var allTime time.Duration
@@ -138,30 +145,56 @@ func outputFuncDifferentTurn(file *excelize.File) {
 	arrayTurn := []string{"P", "W", "R", "O"}
 	fmt.Println(arrayTurn)
 
+	// Запись заколовка csv файла
+	if err := writerCSV.Write([]string{"start_combination", "func", "time", "system", "start_property"}); err != nil {
+		panic(err)
+	}
+
 	file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), arrayTurn)
 	indexExcle++
 
+	// Начало последовательного выполнения функций
 	funcTime = printScreen(ITERATION_LOOP)
 	file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Вывод на экран | время: ")
 	file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+	if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Вывод на экран", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+		panic(err)
+	}
+
 	allTime += funcTime
 	indexExcle++
 
 	funcTime = writeFile(ITERATION_LOOP)
 	file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Запись в файл | время: ")
 	file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+	if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Запись в файл", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+		panic(err)
+	}
+
 	allTime += funcTime
 	indexExcle++
 
 	funcTime = readFile(ITERATION_LOOP)
 	file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Чтение файла | время: ")
 	file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+	if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Чтение файла", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+		panic(err)
+	}
+
 	allTime += funcTime
 	indexExcle++
 
 	funcTime = openDifferentFiles(ITERATION_LOOP)
 	file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Октрытие файлаов разного типа | время: ")
 	file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+	if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Октрытие файлаов разного типа", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+		panic(err)
+	}
+
 	allTime += funcTime
 	indexExcle++
 
@@ -192,6 +225,11 @@ func outputFuncDifferentTurn(file *excelize.File) {
 				funcTime = printScreen(ITERATION_LOOP)
 				file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Вывод на экран | время: ")
 				file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+				if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Вывод на экран", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+					panic(err)
+				}
+
 				allTime += funcTime
 				indexExcle++
 
@@ -199,6 +237,11 @@ func outputFuncDifferentTurn(file *excelize.File) {
 				funcTime = writeFile(ITERATION_LOOP)
 				file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Запись в файл | время: ")
 				file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+				if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Запись в файл", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+					panic(err)
+				}
+
 				allTime += funcTime
 				indexExcle++
 
@@ -206,6 +249,11 @@ func outputFuncDifferentTurn(file *excelize.File) {
 				funcTime = readFile(ITERATION_LOOP)
 				file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Чтение файла | время: ")
 				file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+				if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Чтение файла", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+					panic(err)
+				}
+
 				allTime += funcTime
 				indexExcle++
 
@@ -213,6 +261,11 @@ func outputFuncDifferentTurn(file *excelize.File) {
 				funcTime = openDifferentFiles(ITERATION_LOOP)
 				file.SetCellValue("Sheet1", fmt.Sprintf("A%d", indexExcle), "Октрытие файлаов разного типа | время: ")
 				file.SetCellValue("Sheet1", fmt.Sprintf("B%d", indexExcle), fmt.Sprint(funcTime))
+
+				if err := writerCSV.Write([]string{strings.Join(arrayTurn, ","), "Октрытие файлаов разного типа", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
+					panic(err)
+				}
+
 				allTime += funcTime
 				indexExcle++
 			}
@@ -229,11 +282,29 @@ func outputFuncDifferentTurn(file *excelize.File) {
 }
 
 func main() {
+	// Считывание ключа i и определение ключа sp
 	flag.Parse()
 
+	// Создание excel файла
 	file := excelize.NewFile()
 
-	outputFuncDifferentTurn(file)
+	// Создание CSV файла и его записывателя
+	fileName := "secondary_start_windows_data.csv"
+	if START_PROPERTY_FLAG == 0 {
+		fileName = "primary_start_windows_data.csv"
+	}
+
+	fileCSV, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writerCSV := csv.NewWriter(fileCSV)
+	defer writerCSV.Flush()
+
+	// Запуск основной функции
+	outputFuncDifferentTurn(file, writerCSV)
 
 	if err := file.SaveAs("IntermediaTimeFunc.xlsx"); err != nil {
 		fmt.Println(err)
