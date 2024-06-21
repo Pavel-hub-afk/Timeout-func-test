1c50ee0c045c2e58a5ceaaacf3a2e34554fc6c4e
Pavel <bashmakovhub@gmail.com>
2024-06-21 11:34:44 +0300

    Крайний коммит перед защитой, вытащил все из gitignore

---
+++ paradigms_script/main_functional_for_build.go
@@ -0,0 +1,167 @@
+// Тест времени выполнения функций в разной последовательности.
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/etnz/permute"
+)
+
+// ITERATION_LOOP - переменная итераций цикла. Задается с помощью ключа "-i".
+var ITERATION_LOOP int = 10
+var START_PROPERTY_FLAG int = 1
+
+func init() {
+	flag.IntVar(&ITERATION_LOOP, "i", ITERATION_LOOP, "кол-во итераций цикла")
+	flag.IntVar(&START_PROPERTY_FLAG, "sp", START_PROPERTY_FLAG, "св-во старта")
+}
+
+// createFunc - функция создания функции для выполнения
+func createFunc(f func(int) time.Duration, name string) func() time.Duration {
+	return func() time.Duration {
+		return f(ITERATION_LOOP)
+	}
+}
+
+// printScreen - функция вывода на экран
+func printScreen(l int) time.Duration {
+	t0 := time.Now()
+
+	for i := 0; i < l; i++ {
+		fmt.Println("Hello TEST")
+	}
+
+	t1 := time.Now()
+	fmt.Println("Function print | time: ", t1.Sub(t0))
+	return t1.Sub(t0)
+}
+
+// writeFile - функция записи файла
+func writeFile(l int) time.Duration {
+	t0 := time.Now()
+
+	for i := 0; i < l; i++ {
+		file, err := os.OpenFile("files/test.txt", os.O_WRONLY, 0744)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := file.WriteString("Hello TEST"); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	t1 := time.Now()
+	fmt.Println("Function write | time: ", t1.Sub(t0))
+	return t1.Sub(t0)
+}
+
+// readFile - функция чтения файла
+func readFile(l int) time.Duration {
+	t0 := time.Now()
+
+	for i := 0; i < l; i++ {
+		file, err := os.OpenFile("files/test.txt", os.O_RDONLY, 0744)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		scan := bufio.NewScanner(file)
+		for scan.Scan() {
+			str := scan.Text()
+			if str == "" {
+				// Do nothing for empty lines
+			}
+		}
+
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	t1 := time.Now()
+	fmt.Println("Function read | time: ", t1.Sub(t0))
+	return t1.Sub(t0)
+}
+
+// openDifferentFiles - функция открытия файлов разных типов
+func openDifferentFiles(l int) time.Duration {
+	t0 := time.Now()
+
+	fileTypes := []string{"test.doc", "test.html", "test.txt", "test.jpg"}
+	for i := 0; i < l; i++ {
+		for _, fileType := range fileTypes {
+			file, err := os.OpenFile("files/"+fileType, os.O_RDONLY, 0744)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := file.Close(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	t1 := time.Now()
+	fmt.Println("Function open | time: ", t1.Sub(t0))
+	return t1.Sub(t0)
+}
+
+// outputFuncDifferentTurn - функция вывода времени выполнения функций в разной последовательностии, генерации Excel и CSV файлов
+func outputFuncDifferentTurn() {
+	var allTime time.Duration
+
+	arrayTurn := []string{"P", "W", "R", "O"}
+	funcMap := map[string]func() time.Duration{
+		"P": createFunc(printScreen, "Вывод на экран"),
+		"W": createFunc(writeFile, "Запись в файл"),
+		"R": createFunc(readFile, "Чтение файла"),
+		"O": createFunc(openDifferentFiles, "Открытие файлов разного типа"),
+	}
+	fmt.Println(arrayTurn)
+
+	// Начало последовательного выполнения функций
+	for _, funcKey := range arrayTurn {
+		funcTime := funcMap[funcKey]()
+		allTime += funcTime
+	}
+
+	fmt.Println("All time:", allTime)
+	fmt.Println("------------------------")
+	allTime = 0
+
+	p := permute.NewPlainChangeGen(len(arrayTurn))
+
+	var sw [2]int
+	for p.Next(&sw) {
+
+		permute.SwapStrings(sw, arrayTurn)
+		fmt.Println(arrayTurn)
+
+		for _, funcKey := range arrayTurn {
+			funcTime := funcMap[funcKey]()
+			allTime += funcTime
+		}
+
+		fmt.Println("All time:", allTime)
+		fmt.Println("------------------------")
+		allTime = 0
+	}
+}
+
+func main() {
+	// Считывание ключа i и определение ключа sp
+	flag.Parse()
+
+	// Запуск основной функции
+	outputFuncDifferentTurn()
+}
--- test.go
+++ test.go
@@ -1,248 +1,40 @@
-// Тест времени выплнения функций в разной последовательности.
+import pandas as pd
+import os
+def conv_to_mseconds(str):
+    if str[-2:] == 'ms':
+        return float(str[:-2]) 
+    elif str[-2:] == 'µs':
+        return float(str[:-2]) / 1000
+    elif str[-2:] == '0s':
+        return float(str[:-1])
 
-package main
+path = 'intermedia_CSV_files/'
+csv_files = [file for file in os.listdir(path) if file.endswith('.csv')]
 
-import (
-	"bufio"
-	"encoding/csv"
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"runtime"
-	"strconv"
-	"strings"
-	"time"
+for csv_file in csv_files:
+    df = pd.read_csv(os.path.join(path, csv_file))
 
-	"github.com/etnz/permute"
-)
-
-// ITERATION_LOOP - переменная итераций цикла. Задается с помощью ключа "-i".
-var ITERATION_LOOP int = 10
-var START_PROPERTY_FLAG int = 1
-
-func init() {
-	flag.IntVar(&ITERATION_LOOP, "i", ITERATION_LOOP, "кол-во итераций цикла")
-	flag.IntVar(&START_PROPERTY_FLAG, "sp", START_PROPERTY_FLAG, "св-во старта")
-}
-
-// printScreen - функция вывода на экран
-func printScreen(l int) time.Duration {
-	t0 := time.Now()
-
-	for i := 0; i < l; i++ {
-		fmt.Println("Hello TEST")
-	}
-
-	t1 := time.Now()
-	fmt.Println("Function print | time: ", t1.Sub(t0))
-	return t1.Sub(t0)
-}
-
-// writeFile - функция записи файла
-func writeFile(l int) time.Duration {
-	t0 := time.Now()
-
-	for i := 0; i < l; i++ {
-		file, err := os.OpenFile("files/test.txt", os.O_WRONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if _, err := file.WriteString("Hello TEST"); err != nil {
-			log.Fatal(err)
-		}
-
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	t1 := time.Now()
-	fmt.Println("Function write | time: ", t1.Sub(t0))
-	return t1.Sub(t0)
-}
-
-// readFile - функция чтения файла
-func readFile(l int) time.Duration {
-	t0 := time.Now()
-
-	for i := 0; i < l; i++ {
-		file, err := os.OpenFile("files/test.txt", os.O_RDONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		scan := bufio.NewScanner(file)
-		// TODO: почему-то пустое условие if
-		for scan.Scan() {
-			str := scan.Text()
-			if str == "" {
-
-			}
-		}
-
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	t1 := time.Now()
-	fmt.Println("Function read | time: ", t1.Sub(t0))
-	return t1.Sub(t0)
-}
-
-// openDifferentFiles - функция открытия файлов разных типов
-func openDifferentFiles(l int) time.Duration {
-	t0 := time.Now()
-
-	for i := 0; i < l; i++ {
-		file, err := os.OpenFile("files/test.doc", os.O_RDONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-
-		file, err = os.OpenFile("files/test.html", os.O_RDONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-
-		file, err = os.OpenFile("files/test.txt", os.O_RDONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-
-		file, err = os.OpenFile("files/test.jpg", os.O_RDONLY, 0744)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	t1 := time.Now()
-	fmt.Println("Function open | time: ", t1.Sub(t0))
-	return t1.Sub(t0)
-}
-
-// outputFuncDifferentTurn - функция вывода времени выполнения функций в разной последовательностии, генерации Excel и CSV файлов
-func outputFuncDifferentTurn(writerCSV *csv.Writer) {
-	var funcTime time.Duration
-	var allTime time.Duration
-	var paradigmName string = "imperative"
-
-	arrayTurn := []string{"P", "W", "R", "O"}
-	fmt.Println(arrayTurn)
-
-	// Запись заколовка csv файла
-	if err := writerCSV.Write([]string{"paradigm", "start_combination", "func", "time", "system", "start_property"}); err != nil {
-		panic(err)
-	}
-
-	// Начало последовательного выполнения функций
-	funcTime = printScreen(ITERATION_LOOP)
-	if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "P", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-		panic(err)
-	}
-	allTime += funcTime
-
-	funcTime = writeFile(ITERATION_LOOP)
-	if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "W", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-		panic(err)
-	}
-	allTime += funcTime
-
-	funcTime = readFile(ITERATION_LOOP)
-	if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "R", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-		panic(err)
-	}
-	allTime += funcTime
-
-	funcTime = openDifferentFiles(ITERATION_LOOP)
-	if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "O", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-		panic(err)
-	}
-	allTime += funcTime
-
-	fmt.Println("All time:", allTime)
-	fmt.Println("------------------------")
-	allTime = 0
-
-	p := permute.NewPlainChangeGen(len(arrayTurn))
-
-	var sw [2]int
-	for p.Next(&sw) {
-
-		permute.SwapStrings(sw, arrayTurn)
-		fmt.Println(arrayTurn)
-
-		for _, value := range arrayTurn {
-			switch value {
-
-			case "P":
-				funcTime = printScreen(ITERATION_LOOP)
-				if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "P", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-					panic(err)
-				}
-				allTime += funcTime
-
-			case "W":
-				funcTime = writeFile(ITERATION_LOOP)
-				if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "W", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-					panic(err)
-				}
-				allTime += funcTime
-
-			case "R":
-				funcTime = readFile(ITERATION_LOOP)
-				if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "R", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-					panic(err)
-				}
-				allTime += funcTime
-
-			case "O":
-				funcTime = openDifferentFiles(ITERATION_LOOP)
-				if err := writerCSV.Write([]string{paradigmName, strings.Join(arrayTurn, ","), "O", funcTime.String(), runtime.GOOS, strconv.Itoa(START_PROPERTY_FLAG)}); err != nil {
-					panic(err)
-				}
-				allTime += funcTime
-			}
-		}
-
-		fmt.Println("All time:", allTime)
-		fmt.Println("------------------------")
-		allTime = 0
-	}
-}
-
-func main() {
-	// Считывание ключа i и определение ключа sp
-	flag.Parse()
-
-	// Создание CSV файла и его записывателя
-	fileName := "intermedia_CSV_files/secondary_start_" + runtime.GOOS + "_data.csv"
-	if START_PROPERTY_FLAG == 0 {
-		fileName = "intermedia_CSV_files/primary_start_" + runtime.GOOS + "_data.csv"
-	}
-
-	fileCSV, err := os.Create(fileName)
-	if err != nil {
-		panic(err)
-	}
-
-	writerCSV := csv.NewWriter(fileCSV)
-	defer writerCSV.Flush()
-
-	// Запуск основной функции
-	outputFuncDifferentTurn(writerCSV)
-}
+    df['time'] = df['time'].apply(conv_to_mseconds)
+    df.to_csv(os.path.join(path, csv_file), index=False)
+   
+	# Укажите путь к папке с CSV файлами
+	folder_path = 'intermedia_CSV_files/'
+	
+	# Список для хранения всех отдельных DataFrame
+	data_frames = []
+	
+	# Перебор всех файлов в папке
+	for file_name in os.listdir(folder_path):
+		if file_name.endswith('.csv'):
+			file_path = os.path.join(folder_path, file_name)
+			# Чтение CSV файла и добавление его в список
+			df = pd.read_csv(file_path)
+			data_frames.append(df)
+	
+	# Объединение всех DataFrame в один
+	combined_df = pd.concat(data_frames, ignore_index=True)
+	
+	# Сохранение объединенного DataFrame в новый CSV файл
+	combined_df.to_csv('all_test.csv', index=False)
+	df = pd.read_csv('all_test.csv')
+	df	
